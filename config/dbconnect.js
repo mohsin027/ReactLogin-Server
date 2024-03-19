a98e0f70c5c94847192a6cd5cee576d456fd25eb
mohsin027 <itsmohsin343@gmail.com>
2024-03-19 12:06:36 +0530

    link updated

--- config/dbconnect.js
+++ config/dbconnect.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 function dbConnect(){
     mongoose.set('strictQuery', false);
-    mongoose.connect("mongodb://0.0.0.0:27017/crud").then(()=>{
+    mongoose.connect(process.env.MONGO_URL).then(()=>{
         console.log("db connected")
     }).catch(err=>{
         console.log(err)
